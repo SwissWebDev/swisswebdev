dbc4c86a0550d15b816b30275f729e955ae65413
MarvinK <marvin.kiefer@outlook.com>
2025-01-26 18:40:23 +0100

    Added Meta Tags in Header Element

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Swiss Web Dev",
+  title: "Swiss Web Development",
   description: "Swiss Web Development, Design and Consulting",
 };
 
@@ -32,6 +32,55 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="title"
+          content="Swiss Web Development - High-Quality Web Development Services"
+        />
+        <meta
+          name="description"
+          content="Swiss Web Development offers top-notch web development services to help businesses grow online. Contact us today to elevate your digital presence!"
+        />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.swisswebdev.ch/" />
+        <meta
+          property="og:title"
+          content="Swiss Web Development - High-Quality Web Development Services"
+        />
+        <meta
+          property="og:description"
+          content="Swiss Web Development offers top-notch web development services to help businesses grow online. Contact us today to elevate your digital presence!"
+        />
+        {/* <meta
+          property="og:image"
+          content="https://www.swisswebdev.ch/meta-image.jpg"
+        /> */}
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.swisswebdev.ch/" />
+        <meta
+          property="twitter:title"
+          content="Swiss Web Development - High-Quality Web Development Services"
+        />
+        <meta
+          property="twitter:description"
+          content="Swiss Web Development offers top-notch web development services to help businesses grow online. Contact us today to elevate your digital presence!"
+        />
+        {/* <meta
+          property="twitter:image"
+          content="https://www.swisswebdev.ch/meta-image.jpg"
+        /> */}
+
+        {/* Additional Meta Tags */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Swiss Web Dev" />
+        <meta name="theme-color" content="#007ACC" />
+        <link rel="canonical" href="https://www.swisswebdev.ch/" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-auto`}
       >
